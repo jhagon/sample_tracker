d73e63620afb4e81a0dc272fde70e252f453932e
J.P.Hagon <jphagon@gmail.com>
2011-09-18 11:04:03 +0100

    added full dir structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleTracker::Application.config.secret_token = 'ab2d75ef02b480b272255cbf0aa555785b74cddbbb86eee540a28c4403e7387d31331e71f9623672240fad7cde4794d3f20dc2e465ff2f837d2360ee7c11e2de'
